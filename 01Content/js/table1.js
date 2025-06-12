d528650f8c50e3b17b72dd558ac98c36aa11455d
sujin <sunnydayiscoming@gmail.com>
2025-06-12 18:11:03 +0900

    ‘change’

--- 01Content/js/table1.js
+++ 01Content/js/table1.js
@@ -1,6 +1,10 @@
 // 1. HTML 요소들을 찾아서 변수에 저장
-const countElement = document.querySelector('.count');
 const tableRows = document.querySelectorAll('tbody tr');
+const countElement = document.querySelector('.count');
+
+// console.log(tableRows);
+// console.log(countElement);
+// console.log(countElement.outerHTML);
 
 // 2. 화살표 함수로 함수 정의
 const countPosts = () => {
@@ -9,7 +13,12 @@ const countPosts = () => {
   // 3. 각 tr을 하나씩 확인
   for (let i = 0; i < tableRows.length; i++) {
     const row = document.querySelector(`tbody tr:nth-child(${i + 1})`);
+    // console.log(row);
+
     const firstRow = document.querySelector(`tbody tr:nth-child(${i + 1}) td:first-child`);
+
+    //일일이 체크
+    console.log(firstRow.innerHTML);
 
     if (firstRow.textContent.includes('현재 게시물이 존재하지 않습니다')) {
       continue;
